b75ce22e7ca714110911dacd9f53e0e6b69b22e2
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-04-08 00:45:22 +0530

    added basic file structure
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

---
+++ src/Components/SideBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class SideBar extends React.Component {
+
+    render() {
+
+        return (
+
+            <div className="side_bar">
+
+                <li>New</li>
+                <li>DashBoard</li>
+                <li>Lists</li>
+                <li>Transaction</li>
+                <li>Reports</li>
+
+            </div>
+        )
+    }
+}
+
+export default SideBar
---
+++ src/Containers/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import SideBar from '../Components/SideBar';
+
+class App extends React.Component {
+
+    render() {
+
+        return (
+
+            <div>
+
+                HIii
+
+
+
+            </div>
+        )
+    }
+}
+
+export default App
