e88a433a9e8d194f097f5d1c521584092973c93a
Tushar <iamtushar324@gmail.com>
2020-10-18 20:43:49 +0530

    Added agency basic routings
    Signed-off-by: Tushar <iamtushar324@gmail.com>

--- src/containers/main/SideBar.js
+++ src/containers/main/SideBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import DashBoardIcon from "assets/icons/dashboard1.svg";
 import AccountingIcon from "assets/icons/money.svg";
 import TransactionsIcon from "assets/icons/transaction.svg";
@@ -6,107 +6,71 @@ import ReportsIcon from "assets/icons/reports.svg";
 import AgencyIcon from "assets/icons/agency.svg";
 import TrashIcon from "assets/icons/trash.svg";
 import sample from "assets/icons/user.svg"
+import {Link} from "react-router-dom"
+import { useLocation } from 'react-router-dom'
 
-class SideBar extends React.Component {
-  navToDash() {
-    this.props.navTo("dash");
-  }
-  navToRep() {
-    this.props.navTo("rep");
-  }
-  navToAcc() {
-    this.props.navTo("accounting");
-  }
-  navToTrans() {
-    this.props.navTo("trans");
-  }
+export default function SideBar ({match}) {
+const [activeBtn , setActiveBtn ] = useState("dashboard")
+const location = useLocation();
 
-  constructor(props) {
-    super(props);
-    this.navToDash = this.navToDash.bind(this);
-    this.navToRep = this.navToRep.bind(this);
-    this.navToAcc = this.navToAcc.bind(this);
-    this.navToTrans = this.navToTrans.bind(this);
-  }
-
-  render() {
     return (
       <div className="side_bar">
         <div className="side_bar_con">
           <img className="add_img_btn" src={sample} alt="" />
           
 
-          <li className="new_btn" onClick={this.props.AddAccFromUsersCrossBtn}>
+          <li className="new_btn" >
             <span>New</span> <img src="" alt="" />
           </li>
 
-          <li className={this.props.actPage === "dash" ? "side_btn act_s_btn" : "side_btn"} onClick={this.navToDash}>
+            <Link to="/agency/dashboard">
+              <li className={location.pathname==="/agency/dashboard" ? "side_btn act_s_btn" : "side_btn"} >
             <img src={DashBoardIcon} alt="" /> <span>DashBoard</span>
             <div className="side_pop" />
           </li>
+            </Link>
+
+            <Link to="/agency/accountings">
           <li
-            className={this.props.actPage === "accounting" ? "side_btn act_s_btn" : "side_btn"}
-            onClick={() => {
-              this.navToAcc();
-            }}
+            className={location.pathname==="/agency/accountings"? "side_btn act_s_btn" : "side_btn"}
           >
             <img src={AccountingIcon} alt="" />
             <span>Accounting</span>
             <img src="" alt="" />
             <div className="side_pop">
-              <li
-                onClick={() => {
-                  this.props.setProOrAcc("Accounts");
-                }}
-              >
+              <li>
                 Accounts
               </li>
-              <li
-                onClick={() => {
-                  this.props.setProOrAcc("Products");
-                }}
-              >
+              <li>
                 Products
               </li>
             </div>
           </li>
+            </Link>
+            <Link to="/agency/transactions">
           <li
-            className={this.props.actPage === "trans" ? "side_btn act_s_btn" : "side_btn"}
-            onClick={() => {
-              this.navToTrans();
-            }}
+            className={location.pathname==="/agency/transactions" ? "side_btn act_s_btn" : "side_btn"}
           >
-            <img src={TransactionsIcon} alt="" />
+            <img src={TransactionsIcon} alt=" " />
             <span>Transactions</span>
             <img src="" alt="" />
             <div className="side_pop">
-              <li
-                onClick={() => {
-                  this.props.setVouchPage("pv");
-                }}
-              >
+              <li>
                 Purchase Voucher
               </li>
-              <li
-                onClick={() => {
-                  this.props.setVouchPage("jv");
-                }}
-              >
+              <li>
                 Journal Voucher
               </li>
-              <li
-                onClick={() => {
-                  this.props.setVouchPage("dn");
-                }}
-              >
+              <li>
                 Debit Note
               </li>
             </div>
           </li>
-          <li className={this.props.actPage === "rep" ? "side_btn act_s_btn" : "side_btn"} onClick={this.navToRep}>
-            <img src={ReportsIcon} alt="" />
+            </Link>
+            <Link to="/agency/reports">
+              <li className={location.pathname==="/agency/reports" ? "side_btn act_s_btn" : "side_btn"} >
+            <img src={ReportsIcon} alt=" " />
             <span>Reports</span>
-            <img src="" alt="" />
             <div className="side_pop">
               <li>Daily book</li>
               <li>Ledger</li>
@@ -114,11 +78,10 @@ class SideBar extends React.Component {
               <li>Commission</li>
             </div>
           </li>
+            </Link>
+            <Link to="/agency/agency">
           <li
-            className={this.props.actPage === "agency" ? "side_btn act_s_btn" : "side_btn"}
-            onClick={() => {
-              this.props.navTo("agency");
-            }}
+            className={location.pathname==="/agency/agency" ? "side_btn act_s_btn" : "side_btn"}
           >
             <img src={AgencyIcon} alt="" />
             <span>Agency</span>
@@ -128,20 +91,18 @@ class SideBar extends React.Component {
               <li>Products</li>
             </div>
           </li>
+            </Link>
+            <Link to="/agency/trash">
           <hr className="hr_line" />
           <li
-            className="side_btn"
-            onClick={() => {
-              this.props.navTo("trash");
-            }}
+            className={location.pathname==="/agency/trash" ? "side_btn act_s_btn" : "side_btn"}
           >
             <img src={TrashIcon} alt="" />
             Trash
           </li>
+            </Link>
         </div>
       </div>
     );
   }
-}
 
-export default SideBar;
