ef51de78936899d8413e82a44ed9c54726ce9035
Tushar <iamtushar324@gmail.com>
2021-03-12 15:53:55 +0530

    Added proxy to our dev server backend
    Signed-off-by: Tushar <iamtushar324@gmail.com>

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  app.use(createProxyMiddleware("/api/*", { target: "http://localhost:5000", secure: false }));
+  app.use(
+    createProxyMiddleware("/api/*", { target: "https://dev.accute.live", secure: true, changeOrigin:true ,rejectUnauthorized: false })
+  );
 };
